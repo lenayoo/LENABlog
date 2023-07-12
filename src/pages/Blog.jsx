1e0eb8ee84d02f2c2b5e0d3cb5058dcb655cae09
Lena Yoo <lenayoo17@gmail.com>
2023-07-12 09:12:27 +0900

    add blog structure

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -8,6 +8,16 @@ function Blog() {
         <NavBar />
       </div>
       <h1>Blog</h1>
+      <form className="blog-form">
+        <label>Title</label>
+        <input className="blog-title" />
+        <label>Date</label>
+        <input className="blog-date" />
+        <label>Content</label>
+        <input className="blog-content" />
+        <button>저장하기</button>
+      </form>
+      <div className="underline"></div>
     </div>
   )
 }
