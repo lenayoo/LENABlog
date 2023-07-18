f8791e1819747473c4cae4e3d8bc31b74e3a9943
Lena Yoo <lenayoo17@gmail.com>
2023-07-18 09:27:36 +0900

    add til changeHandler

--- src/pages/Til.jsx
+++ src/pages/Til.jsx
@@ -6,21 +6,36 @@ import 'react-datepicker/dist/react-datepicker.css'
 
 function TiL() {
   const [selectedDate, setSelectedDate] = useState(null)
+  const [tilInput, setTilInput] = useState('')
+
+  const inputChangeHandler = (e) => {
+    console.log(e.target.value)
+    setTilInput(e.target.value)
+  }
+
+  const submitHandler = () => {
+    
+  }
 
   return (
-    <div className="page-body">
+    <div className="page-body til">
       <div className="page-header">
         <NavBar />
       </div>
       <h1>Til</h1>
-      <form className="til-form">
+      <form className="til-form" onSubmit={submitHandler}>
         <label>Date</label>
         <DatePicker
+          className="til-date"
           selected={selectedDate}
           onChange={(date) => setSelectedDate(date)}
         />
         <label className="til-label">What I did for today</label>
-        <input className="til-input" />
+        <input
+          className="til-input"
+          value={tilInput}
+          onChange={inputChangeHandler}
+        />
         <button type="submit">save</button>
       </form>
       <div className="underline"></div>
