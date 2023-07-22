1689949e4c125b192cbbca634f4a5babf3348602
Lena Yoo <lenayoo17@gmail.com>
2023-07-22 11:47:48 +0900

    finish about section

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -8,7 +8,17 @@ function About() {
         <NavBar />
       </div>
       <h1>About</h1>
-      <p>About programmer Lena</p>
+      <div className="about-intro">
+        <p>
+          Hi, I am Lena. I am react programmer. <br />I love to create the
+          things to help me and others to find their ways. sound too big?
+          <br />
+          Currently, I live in Tokyo, Japan and work as a frontend engineer.{' '}
+          <br />
+          But who will dare to know that what kind of engineer I am going to be
+          in 5 years?
+        </p>
+      </div>
     </div>
   )
 }
