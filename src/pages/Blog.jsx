479bae123e18358fd23ee5fc7b04b8336638a4ba
Lena Yoo <lenayoo17@gmail.com>
2023-07-22 10:17:01 +0900

    fix value error for blog section

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,7 +1,27 @@
+import { useState } from 'react'
 import NavBar from '../NavBar'
 import './style.css'
 
 function Blog() {
+  const [title, setTitle] = useState('')
+  const [date, setDate] = useState(null)
+  const [content, setContent] = useState('')
+
+  const titleChangeHandler = (e) => {
+    console.log(e.target.value)
+    setTitle(e.target.value)
+  }
+
+  const dateChangeHandler = (e) => {
+    console.log(e.target.value)
+    setDate(e.target.value)
+  }
+
+  const contentChangeHandler = (e) => {
+    console.log(e.target.value)
+    setContent(e.target.value)
+  }
+
   return (
     <div className="page-body">
       <div className="page-header">
@@ -10,11 +30,27 @@ function Blog() {
       <h1>Blog</h1>
       <form className="blog-form">
         <label>Title</label>
-        <input className="blog-title" />
+        <input
+          className="blog-title"
+          name="title"
+          value={title}
+          onChange={titleChangeHandler}
+        />
         <label htmlFor="date">Date</label>
-        <input className="blog-date" type="date" />
+        <input
+          className="blog-date"
+          type="date"
+          name="date"
+          value={date}
+          onInput={dateChangeHandler}
+        />
         <label>Content</label>
-        <input className="blog-content" />
+        <input
+          className="blog-content"
+          name="content"
+          value={content}
+          onChange={contentChangeHandler}
+        />
         <button>저장하기</button>
       </form>
       <div className="underline"></div>
